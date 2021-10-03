18d80852342b21283820a7d2f1a5e88bc5f0e2b1
Jin Zhou He <jinzhou@cs.ualberta.ca>
2021-10-03 17:02:23 -0600

    Get complete

--- httpclient.py
+++ httpclient.py
@@ -33,21 +33,52 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    # get host and port of the url you want 
+    def get_host_port(self,url):
+        block = (urllib.parse.urlparse(url))
+        scheme = block.scheme
+        port = block.port
+        host = block.hostname
 
+        # port 80 for Http and 443 for Https scheme or the url already contains host and port
+        if scheme == "http" and port == None:
+            return host, 80
+        else:
+            return host, port
+
+    # Constructing GET request to send to server 
+    def request_data(self, url, host_name):
+        block = (urllib.parse.urlparse(url))
+        path = block.path
+        if path == None:
+            path = "/"
+        else:
+            path
+        payload = f'GET {path} HTTP/1.1\r\nHost: {host_name}\r\nAccept: */*\r\nConnection: close\r\n\r\n'
+        print("payload constructed")
+        return payload
+
+    # Constructing POST to the server
+    def post_data(self, url, host_name):
+        pass
+
+    # creating the connection once we have host and port
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
         return None
 
+    # get status code
     def get_code(self, data):
-        return None
+        return int(data.split()[1])
 
+    # get headers of response
     def get_headers(self,data):
-        return None
+        return data.split("\r\n\r\n")[0]
 
+    # get body of response
     def get_body(self, data):
-        return None
+        return data.split("\r\n\r\n")[1]
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -70,7 +101,24 @@ class HTTPClient(object):
     def GET(self, url, args=None):
         code = 500
         body = ""
-        return HTTPResponse(code, body)
+
+        host_name, client_port = self.get_host_port(url)
+        self.connect(host_name, client_port)
+        request = self.request_data(url, host_name)
+        # send to server
+        self.sendall(request)
+        # respond from server
+        response = self.recvall(self.socket)
+
+        status_code = self.get_code(response)
+        headers = self.get_headers(response)
+        body = self.get_body(response)
+
+        print(status_code)
+        print(headers)
+        print(body)
+        self.close()
+        return HTTPResponse(status_code, body)
 
     def POST(self, url, args=None):
         code = 500
@@ -79,9 +127,12 @@ class HTTPClient(object):
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
+            print("Post")
             return self.POST( url, args )
         else:
+            print("Get")
             return self.GET( url, args )
+        
     
 if __name__ == "__main__":
     client = HTTPClient()
@@ -90,6 +141,8 @@ if __name__ == "__main__":
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
+        print("Method and URL")
         print(client.command( sys.argv[2], sys.argv[1] ))
     else:
+        print("Just Method")
         print(client.command( sys.argv[1] ))
